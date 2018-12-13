fac4ad24d3590510c84d83ba2483f67c1d4cf246
NTDOMAIN\nihadk <nihadkeric@gmail.com>
2018-12-13 12:59:47 +0100

    Use: Lifecycle, DOM Access, set Attributes and Styling

--- tooltip.js
+++ tooltip.js
@@ -1,9 +1,44 @@
+// Create the class Tooltip with extend HTMLElement
 class Tooltip extends HTMLElement {
 
     constructor(){
         super();
-        console.log("It is working...");
+        this._tooltipContainer;
+        this._tooltipText = 'Some dummy tooltip text.';
     }
+
+    // lifecrycle methods
+    connectedCallback() {
+        if(this.hasAttributes('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this.appendChild(tooltipIcon);
+        this.style.position = 'relative';
+    }
+
+    // private methods
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+        this._tooltipContainer.style.backgroundColor = 'green';
+        this._tooltipContainer.style.color = 'white';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = 10;
+
+
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.removeChild(this._tooltipContainer);
+    }
+
+
 }
 
+// definde the name of the HTML component and add the class (for exp. Tooltip)
 customElements.define('nk-tooltip', Tooltip);
