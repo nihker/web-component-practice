9cfcc3ffd3515279d8a6eeffe676240d6b0be83b
NTDOMAIN\nihadk <nihadkeric@gmail.com>
2018-12-13 15:56:29 +0100

    Styling the slote in the custome element

--- tooltip.js
+++ tooltip.js
@@ -18,6 +18,14 @@ class Tooltip extends HTMLElement {
                     position: absolute;
                     z-index: 10;
                 }
+
+                .highlight {
+                    background-color: red;
+                }
+
+                ::slotted(.highlight) {
+                    border-bottom: 1px dotted red;
+                }
             </style>
             <span> (?)</span>
             <slot>Some Default</slot>
