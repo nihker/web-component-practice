211cf7422f6d363aca213b11f376f6cd38280aba
NTDOMAIN\nihadk <nihadkeric@gmail.com>
2018-12-14 14:26:52 +0100

    Public method and properties

--- modal.js
+++ modal.js
@@ -3,6 +3,7 @@ class Modal extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -97,9 +98,23 @@ class Modal extends HTMLElement {
         `;
     }
 
+    attributeChangeCallback(name, oldValue, newValue) {
+        if (name === 'opened') {
+            if (this.hasAttribute('opened')) {
+                this.isOpen = true;
+            }
+        } else {
+            this.isOpen = false;
+        }
+    }
+
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
     open() {
-        console.log('Call button 2');
         this.setAttribute('opened', '');
+        this.isOpen = true;
     }
 }
 
