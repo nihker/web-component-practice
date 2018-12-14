480907fdff09681bad43d55f44b775b08f97d46f
NTDOMAIN\nihadk <nihadkeric@gmail.com>
2018-12-14 10:18:39 +0100

    I filled the template and added the style

--- modal.js
+++ modal.js
@@ -3,9 +3,22 @@ class Modal extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = ``;
+        this.shadowRoot.innerHTML = `
+            <style>
+                #backdrop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100vh;
+                    background: rgba(0,0,0,0.75);
+                }
+            </style>
+            <div id="backdrop"></div>
+            <div id="modal"></div>
+        `;
     }
 
 }
 
-customElements.define('nk-model', Modal);
+customElements.define('nk-modal', Modal);
