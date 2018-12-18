b69f99436a1e0c459606b826701de5c9a2afa142
nihker <nihadkeric@gmail.com>
2018-12-18 07:38:09 +0100

    Create new component and added style and props

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,24 @@
+import { Component, Prop } from "@stencil/core";
+
+
+@Component({
+    tag: 'nk-side-drawer',
+    styleUrl: 'side-drawer.css',
+    shadow: true
+})
+export class SideDrawer {
+    @Prop({
+        reflectToAttr: true
+    }) title: string;
+
+    render () {
+        return (
+            <aside>
+                <header><h1>{ this.title }</h1></header>
+                <main>
+                    <slot />
+                </main>
+            </aside>
+        );
+    }
+}
