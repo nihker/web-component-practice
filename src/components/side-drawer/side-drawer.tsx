ea0f9ae0f32259335615ad2bdd3ee140c077a87d
nihker <nihadkeric@gmail.com>
2018-12-19 07:44:51 +0100

    Added the showContactInfo state variable and implement the logic

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from "@stencil/core";
+import { Component, Prop, State } from "@stencil/core";
 
 
 @Component({
@@ -7,44 +7,49 @@ import { Component, Prop } from "@stencil/core";
     shadow: true
 })
 export class SideDrawer {
+
+    @State() showContactInfo = false;
+
     @Prop({ reflectToAttr: true }) title: string;
     @Prop({ reflectToAttr: true, mutable: true }) open: boolean;
 
-    onCloseDrawer () {
+    onCloseDrawer() {
         console.log("Close Drawer inside the Component....");
         this.open = false;
     }
 
-    onContentChange(content: string){
-        console.log(content);
+    onContentChange(content: string) {
+        this.showContactInfo = content === 'contact';
     }
 
-    render () {
-       let mainContent = <slot />
-       mainContent = (
-            <div id="contact-info">
-                <h2>Contact Information</h2>
-                <p>You can reach us via phone or email</p>
-                <ul>
-                    <li>Phone: 54856464648</li>
-                    <li>Email: <a href="mailto:test@test.com">test@test.com</a></li>
-                </ul>
-            </div>
-       );
+    render() {
+        let mainContent = <slot />
+        if (this.showContactInfo) {
+            mainContent = (
+                <div id="contact-info">
+                    <h2>Contact Information</h2>
+                    <p>You can reach us via phone or email</p>
+                    <ul>
+                        <li>Phone: 54856464648</li>
+                        <li>Email: <a href="mailto:test@test.com">test@test.com</a></li>
+                    </ul>
+                </div>
+            );
+        }
         return (
             <aside>
-                    <header>
-                        <h1>{ this.title }</h1>
-                        <button onClick={ this.onCloseDrawer.bind(this) }>X</button>
-                    </header>
-                    <section id="tabs">
-                        <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
-                        <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
-                    </section>
-                    <main>
-                      { mainContent }
-                    </main>
-                </aside>
+                <header>
+                    <h1>{this.title}</h1>
+                    <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+                </header>
+                <section id="tabs">
+                    <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+                    <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+                </section>
+                <main>
+                    {mainContent}
+                </main>
+            </aside>
         );
     }
 }
