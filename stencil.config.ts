b1d52a083df51f4d78583cda25f0e937123ec795
nihker <nihadkeric@gmail.com>
2018-12-21 12:54:53 +0100

    Update config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,13 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
+  namespace: 'SPWebComponent',
   outputTargets:[
     { type: 'dist' },
-    { type: 'docs' },
-    {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
+    // { type: 'docs' },
+    // {
+    //   type: 'www',
+    //   serviceWorker: null // disable service workers
+    // }
   ]
 };
